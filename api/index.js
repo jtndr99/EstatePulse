130f10c2143adeb02bff82088cac76d9e29ef2e8
jtndr99 <jitmutreja20@gmail.com>
2023-12-24 02:43:35 +0530

    SignUp and auth

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from './routes/user.route.js';
+import authRouter from  './routes/auth.route.js';
 
 dotenv.config();
 
@@ -13,8 +15,26 @@ mongoose
     console.log(err);
   });
 
+
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("server is running!");
 });
+
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
+
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message,
+    });
+  });
