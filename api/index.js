fd654a10615e41fdd28f770f57dc3daf6eadab42
jtndr99 <jitmutreja20@gmail.com>
2023-12-23 22:40:17 +0530

    Connected to db

---
+++ api/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.USER)
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("server is running!");
+});
