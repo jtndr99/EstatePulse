158fb679688d454fed82f8af81ceff4e65497e5e
jtndr99 <jitmutreja20@gmail.com>
2023-12-24 20:42:53 +0530

    User Validate

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,24 +1,50 @@
 import User from "../models/user.model.js";
-import bcryptjs from "bcryptjs"; 
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { errorHandler } from "../utils/error.js";
 
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
 
+  const hashedPassword = bcryptjs.hashSync(password, 12);
 
-export const signup = async(req, res) => {
+  const newUser = new User({ username, email, password: hashedPassword });
 
-    const {username, email, password} = req.body;
+  try {
+    await newUser.save();
+    res.status(201).json("User created success");
+    
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+};
 
-    const hashedPassword = bcryptjs.hashSync(password, 12);
+export const signin = async (req, res, next) => {
+  
+  const { email, password } = req.body;
+  
+  try {
 
-    const newUser = new User({username, email, password: hashedPassword});
+    const validUser = await User.findOne({ email });
 
-    try {
-        await newUser.save();
-
-        res.status(201).json("User created success");
-
-    } catch (error) {
-        res.status(500).json(error.message);
+    if (!validUser) {
+        return next(errorHandler(404, "User not found!"));
     }
 
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+
+    if (!validPassword) {
+        return next(errorHandler(401, "Dare you enter wrong info!!"));
+    }
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
     
-}
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
--- api/index.js
+++ api/index.js
@@ -17,7 +17,7 @@ mongoose
 
 
 const app = express();
-app.use(express.json());
+app.use(express.json());                                                                           // helps in sending req from insomnia api
 
 app.listen(3000, () => {
   console.log("server is running!");
