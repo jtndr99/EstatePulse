b4590888088a74e96b03d1572756c3ec66aaeac0
jtndr99 <jitmutreja20@gmail.com>
2023-12-27 22:06:16 +0530

    Users can update their profile

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from './routes/user.route.js';
 import authRouter from  './routes/auth.route.js';
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -17,7 +18,9 @@ mongoose
 
 
 const app = express();
-app.use(express.json());                                                                           // helps in sending req from insomnia api
+
+app.use(express.json());     
+app.use(cookieParser());                                                                      // helps in sending req from insomnia api
 
 app.listen(3000, () => {
   console.log("server is running!");
